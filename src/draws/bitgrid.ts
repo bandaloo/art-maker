f3fd4647f545a5d168d3b46821a086edaa4351d8
bandaloo <bandaloo@icloud.com>
2020-10-05 20:05:05 -0400

    size varying bit grid

--- src/draws/bitgrid.ts
+++ src/draws/bitgrid.ts
@@ -1,7 +1,8 @@
 import { ChanceTable } from "../chancetable";
 import { H, V, S, C, R, DrawFunc, mix, TupleVec3, randBetween } from "../utils";
 
-type CellFunc = (i: number, j: number) => number;
+type ColorFunc = (i: number, j: number) => number;
+type SizeFunc = (i: number, j: number, t: number) => number;
 
 export function bitGrid(): DrawFunc {
   const r = () => Math.random() * 255;
@@ -9,6 +10,14 @@ export function bitGrid(): DrawFunc {
   const color2: TupleVec3 = [r(), r(), r()];
   const hNum = Math.floor(randBetween(15, 40));
   const vNum = Math.floor(randBetween(15, 40));
+
+  // TODO make this into a utility function
+  const b = Math.floor(Math.random() * 2) * 255;
+  const background = R(b, b, b);
+  const clearBackground = (x: CanvasRenderingContext2D) => {
+    x.fillStyle = background;
+    x.fillRect(0, 0, H, V);
+  };
 
   const smooth = Math.random() > 0.2;
 
@@ -27,14 +36,33 @@ export function bitGrid(): DrawFunc {
     return (i: number, j: number) => (((i - j) ^ (j + i)) / div) % m;
   })();
 
-  const chanceTable = new ChanceTable<CellFunc>();
-  chanceTable.addAll([
+  const colorFuncTable = new ChanceTable<ColorFunc>();
+  colorFuncTable.addAll([
     [xorFunc, 1],
     [andFunc, 2],
     [plusMinusXorFunc, 1],
   ]);
+  const colorFunc = colorFuncTable.pick();
 
-  const chosenFunc = chanceTable.pick();
+  const sinFunc = (() => {
+    const xAmp = randBetween(0.3, 1.5);
+    const yAmp = randBetween(0.3, 1.5);
+    const xSpeed = randBetween(0.3, 2);
+    const ySpeed = randBetween(0.3, 2);
+    const xFreq = randBetween(1, 9);
+    const yFreq = randBetween(1, 9);
+    return (i: number, j: number, t: number) =>
+      xAmp * Math.sin(xSpeed * t + xFreq * i) +
+      yAmp * Math.cos(ySpeed + t + yFreq * j);
+  })();
+  const oneFunc = () => 1;
+
+  const sizeFuncTable = new ChanceTable<SizeFunc>();
+  sizeFuncTable.addAll([
+    [sinFunc, 3],
+    [oneFunc, 1],
+  ]);
+  const sizeFunc = sizeFuncTable.pick();
 
   const hSize = H / hNum;
   const vSize = V / vNum;
@@ -50,14 +78,21 @@ export function bitGrid(): DrawFunc {
     x: CanvasRenderingContext2D,
     c: HTMLCanvasElement
   ) => {
+    clearBackground(x);
     for (let i = 0; i < hNum + 1; i++) {
       const ri = Math.floor(i + t * iSpeed);
       const iOffset = smooth ? (t * iSpeed) % 1 : 0;
       for (let j = 0; j < vNum + 1; j++) {
         const rj = Math.floor(j + t * jSpeed);
         const jOffset = smooth ? (t * jSpeed) % 1 : 0;
-        x.fillStyle = R(...mix(color1, color2, chosenFunc(ri, rj)));
-        x.fillRect((i - iOffset) * hSize, (j - jOffset) * vSize, hSize, vSize);
+        const size = sizeFunc(ri, rj, t);
+        x.fillStyle = R(...mix(color1, color2, colorFunc(ri, rj)));
+        x.fillRect(
+          (i - iOffset) * hSize,
+          (j - jOffset) * vSize,
+          hSize * size,
+          vSize * size
+        );
       }
     }
   };
